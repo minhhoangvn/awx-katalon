4424d721ec6e4c2640ef0762be091e43172ed030
minhhoang <minhhoang@kms-technology.com>
2020-08-05 10:43:54 +0700

    Init

---
+++ katalon-demo/katalon-awx/Test Suites/TestSuite-01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Pass</description>
+   <name>TestSuite-01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bfea5e6b-d60d-475f-af33-58cc645ffc4b</testSuiteGuid>
+   <testCaseLink>
+      <guid>6c663bb3-161c-45ba-a9ab-742af57ef460</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-01</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ katalon-demo/katalon-awx/Test Suites/TestSuite-02.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite-02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5321f9f5-4edc-4eb0-b0cc-12470afb3845</testSuiteGuid>
+   <testCaseLink>
+      <guid>71df213a-3169-4460-8e95-c4316ed55071</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
